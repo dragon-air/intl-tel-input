002b51f4c604f269ab964b976b97e3d531785b9b
dragon-air <sourabhkr.pasi@gmail.com>
2016-03-22 13:48:02 +0530

    Update intlTelInput.js

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -17,7 +17,7 @@
 })(function($, window, document, undefined) {
     "use strict";
     
-      var myVar = 123;
+      var myVar = {{fragmentData.code_sent_info.display}};
         console.log(myVar, "Hello, world!");
     // these vars persist through all instances of the plugin
     var pluginName = "intlTelInput", id = 1, // give each instance it's own id for namespaced event handling
