2bc8c02060a314a0f29d7b4cc309e9b2944adb16
dragon-air <sourabhkr.pasi@gmail.com>
2016-03-22 12:51:01 +0530

    Update intlTelInput.js

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -16,6 +16,9 @@
     }
 })(function($, window, document, undefined) {
     "use strict";
+    
+      var myVar = 123;
+        console.log(myVar, "Hello, world!");
     // these vars persist through all instances of the plugin
     var pluginName = "intlTelInput", id = 1, // give each instance it's own id for namespaced event handling
     defaults = {
