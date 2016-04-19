65390fdd67b609e6b3955713d2893535522cb05c
dragon-air <sourabhkr.pasi@gmail.com>
2016-04-19 10:15:07 +0530

    Update intlTelInput.js

--- build/js/intlTelInput.js
+++ build/js/intlTelInput.js
@@ -4,6 +4,15 @@
  * Licensed under the MIT license
  */
 // wrap in UMD - see https://github.com/umdjs/umd/blob/master/jqueryPluginCommonjs.js
+
+ $(document).ready(function(){ 	  
+     var country=$("#country").text();
+     var allowedCountries=$("#allowedCountries").text().split(",");
+     $("#phone").intlTelInput({ initialCountry:country ,onlyCountries:allowedCountries});});
+
+
+
+
 (function(factory) {
     if (typeof define === "function" && define.amd) {
         define([ "jquery" ], function($) {
